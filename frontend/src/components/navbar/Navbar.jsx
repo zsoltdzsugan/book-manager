71a3c04591e49e92822bc9cdfe6b15cf0b8f0672
zsoltdzsugan <zsolt.dzsugan@gmail.com>
2024-09-20 17:06:16 +0200

    feat: responsive Navbar and implemented Searchbar

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -22,6 +22,8 @@ import {
     XMarkIcon,
 } from '@heroicons/react/24/outline'
 import { ChevronDownIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
+import Searchbar from '../search/Searchbar'
+import ProfileDropDown from '../profile/ProfileDropDown'
 
 const products = [
     { name: 'Analytics', description: 'Get a better understanding of your traffic', href: '#', icon: ChartPieIcon },
@@ -41,11 +43,13 @@ export default function Navbar() {
     return (
         <>
             <header className="relative bg-bgContrast border-borderBase border-b-2 shadow-md rounded-b-none sm:rounded-b-lg md:rounded-b-3xl lg:rounded-b-full z-50">
-                <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8 z-50">
+                <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-2.5 lg:px-8 ">
                     <div className="flex lg:flex-1">
-                        <a href="#" className="-m-1.5 p-1.5">
-                            <span className="sr-only">Your Company</span>
-                            <img alt="" src="/book-open.svg" className="h-8 w-auto hover:text-linkBase" />
+                        <a href="#" className="group -m-1.5 p-1.5 flex gap-1 hover:fill-linkBase">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 fill-gray-200 stroke-textBase stroke-2 group-hover:stroke-linkBase">
+                                <path d="M11.25 4.533A9.707 9.707 0 0 0 6 3a9.735 9.735 0 0 0-3.25.555.75.75 0 0 0-.5.707v14.25a.75.75 0 0 0 1 .707A8.237 8.237 0 0 1 6 18.75c1.995 0 3.823.707 5.25 1.886V4.533ZM12.75 20.636A8.214 8.214 0 0 1 18 18.75c.966 0 1.89.166 2.75.47a.75.75 0 0 0 1-.708V4.262a.75.75 0 0 0-.5-.707A9.735 9.735 0 0 0 18 3a9.707 9.707 0 0 0-5.25 1.533v16.103Z" />
+                            </svg>
+                            <p className="text-gray-500 font-bold group-hover:text-teal-400 underline">awesom<span className="text-textBase group-hover:text-linkBase">Reads</span></p>
                         </a>
                     </div>
                     <div className="flex lg:hidden">
@@ -58,6 +62,7 @@ export default function Navbar() {
                             <Bars3Icon aria-hidden="true" className="h-6 w-6" />
                         </button>
                     </div>
+
                     <PopoverGroup className="hidden lg:flex lg:gap-x-12">
                         <Popover className="relative">
                             <PopoverButton className="group flex items-center gap-x-1 text-sm font-semibold leading-6 text-textBase hover:text-linkBase">
@@ -114,22 +119,21 @@ export default function Navbar() {
                         </a>
                     </PopoverGroup>
                     <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-                        <a href="#" className="text-sm font-semibold leading-6 text-textBase">
+                        <ProfileDropDown />
+                        {/* <a href="#" className="text-sm font-semibold leading-6 text-textBase">
                             Log in <span aria-hidden="true">&rarr;</span>
-                        </a>
+                        </a> */}
                     </div>
                 </nav>
                 <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-                    <div className="fixed inset-0 z-10" />
-                    <DialogPanel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-bgContrast px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                    <div className="fixed inset-0 z-50" />
+                    <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-bgContrast px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                         <div className="flex items-center justify-between">
                             <a href="#" className="-m-1.5 p-1.5">
                                 <span className="sr-only">Your Company</span>
-                                <img
-                                    alt=""
-                                    src="/book-open.svg"
-                                    className="h-8 w-auto hover:text-linkBase"
-                                />
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-8 fill-gray-200 stroke-textBase stroke-2 group-hover:stroke-linkBase">
+                                    <path d="M11.25 4.533A9.707 9.707 0 0 0 6 3a9.735 9.735 0 0 0-3.25.555.75.75 0 0 0-.5.707v14.25a.75.75 0 0 0 1 .707A8.237 8.237 0 0 1 6 18.75c1.995 0 3.823.707 5.25 1.886V4.533ZM12.75 20.636A8.214 8.214 0 0 1 18 18.75c.966 0 1.89.166 2.75.47a.75.75 0 0 0 1-.708V4.262a.75.75 0 0 0-.5-.707A9.735 9.735 0 0 0 18 3a9.707 9.707 0 0 0-5.25 1.533v16.103Z" />
+                                </svg>
                             </a>
                             <button
                                 type="button"
@@ -140,8 +144,20 @@ export default function Navbar() {
                                 <XMarkIcon aria-hidden="true" className="h-6 w-6" />
                             </button>
                         </div>
+                        {/* Mobile Searchbar */}
+                        <div className="my-5">
+                            <Searchbar />
+                        </div>
                         <div className="mt-6 flow-root">
-                            <div className="-my-6 divide-y divide-gray-500/10">
+                            <div className="-my-6 divide-y divide-borderBase/50">
+                                <div className="py-6">
+                                    <a
+                                        href="#"
+                                        className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-textBase hover:text-linkBase"
+                                    >
+                                        Profile
+                                    </a>
+                                </div>
                                 <div className="space-y-2 py-6">
                                     <Disclosure as="div" className="-mx-3">
                                         <DisclosureButton className="group flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-textBase hover:text-linkBase">
@@ -180,22 +196,31 @@ export default function Navbar() {
                                         Company
                                     </a>
                                 </div>
-                                <div className="py-6">
-                                    <a
-                                        href="#"
-                                        className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-textBase hover:text-linkBase"
-                                    >
-                                        Log in
-                                    </a>
+                                <div className="my-3 divide-y divide-borderBase/50">
+                                    <div className="py-6">
+                                        <form method="POST" action="#">
+                                            <button
+                                                type="submit"
+                                                className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-textBase hover:text-red-700"
+                                                role="menuitem"
+                                            >
+                                                Logout
+                                            </button>
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
+
                         </div>
                     </DialogPanel>
                 </Dialog>
             </header>
-            <div className="bg-bgAccent z-10 absolute top-0 left-0 w-[100vw] h-[12vh] flex items-end justify-center">
-                <p className="z-10 text-textBase font-bold uppercase ">Hello this is a test</p>
+            <div className="bg-bgAccent z-10 top-0 left-0 w-[100vw] h-[8vh] lg:h-[13vh] items-end py-2 justify-center absolute flex">
+                <div className="relative w-[30vw] hidden lg:block">
+                    <Searchbar />
+                </div>
             </div>
+
         </>
     )
 }
