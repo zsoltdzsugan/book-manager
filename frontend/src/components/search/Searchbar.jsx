a81b2faea996af76d3cf23fd9c9971c6dac2fb11
zsoltdzsugan <zsolt.dzsugan@gmail.com>
2024-09-20 17:07:45 +0200

    feat: implemented Searchbar

---
+++ frontend/src/components/search/Searchbar.jsx
@@ -0,0 +1,36 @@
+const Searchbar = () => {
+    return (
+        <div className="relative w-full h-10">
+            <label htmlFor="Search" className="sr-only">Search</label>
+
+            <input
+                type="text"
+                id="Search"
+                placeholder="Search for..."
+                className="w-full rounded-2xl border-gray-500 py-2.5 pr-10 shadow-sm sm:text-sm px-3.5 bg-bgAccent lg:bg-bgContrast placeholder:text-gray-500"
+            />
+
+            <span className="absolute inset-y-0 right-0 flex items-center pr-3">
+                <button type="button" className="text-textBase hover:text-linkBase py-1 px-0.5">
+                    <span className="sr-only">Search</span>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="2"
+                        stroke="currentColor"
+                        className="w-5 h-5 stroke-textBase hover:stroke-linkBase"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                        />
+                    </svg>
+                </button>
+            </span>
+        </div>
+    );
+};
+
+export default Searchbar;
