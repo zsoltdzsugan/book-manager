a3e0af65c79949a108d1e58c8159dbf89ff60ce7
zsoltdzsugan <zsolt.dzsugan@gmail.com>
2024-09-22 01:25:17 +0200

    feat: implemented open-close state

--- frontend/src/components/profile/ProfileDropDown.jsx
+++ frontend/src/components/profile/ProfileDropDown.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react"
+
 const ProfileDropDown = () => {
+    const [profileMenuOpen, setProfileMenuOpen] = useState(false);
+
     return (
         <>
             <div className="hidden md:relative md:block">
                 <button
                     type="button"
                     className="overflow-hidden rounded-full border border-gray-300 shadow-inner"
+                    onClick={() => setProfileMenuOpen(!profileMenuOpen)}
                 >
                     <span className="sr-only">Toggle dashboard menu</span>
 
@@ -13,65 +18,68 @@ const ProfileDropDown = () => {
                         alt=""
                         className="size-10 object-cover"
                     />
+
                 </button>
-
-                <div
-                    className="absolute end-0 z-10 mt-0.5 w-56 divide-y divide-bgContrast rounded-md border border-gray-300 bg-bgContrast shadow-lg"
-                    role="menu"
-                >
-                    <div className="p-2">
-                        <a
-                            href="#"
-                            className="block rounded-lg px-4 py-2 text-sm text-textBase hover:bg-bgBase hover:text-linkBase"
-                            role="menuitem"
-                        >
-                            My profile
-                        </a>
-
-                        <a
-                            href="#"
-                            className="block rounded-lg px-4 py-2 text-sm text-textBase hover:bg-bgBase hover:text-linkBase"
-                            role="menuitem"
-                        >
-                            Billing summary
-                        </a>
-
-                        <a
-                            href="#"
-                            className="block rounded-lg px-4 py-2 text-sm text-textBase hover:bg-bgBase hover:text-linkBase"
-                            role="menuitem"
-                        >
-                            Team settings
-                        </a>
-                    </div>
-
-                    <div className="p-2">
-                        <form method="POST" action="#">
-                            <button
-                                type="submit"
-                                className="flex w-full items-center gap-2 rounded-lg px-4 py-2 text-sm text-textBase hover:text-red-700 hover:bg-bgBase"
+                {profileMenuOpen && (
+                    <div
+                        className="absolute end-0 z-10 mt-0.5 w-56 divide-y divide-bgContrast rounded-md border border-gray-300 bg-bgContrast shadow-lg"
+                        role="menu"
+                        onMouseLeave={() => setProfileMenuOpen(false)}
+                    >
+                        <div className="p-2">
+                            <a
+                                href="#"
+                                className="block rounded-lg px-4 py-2 text-sm text-textBase hover:bg-bgBase hover:text-linkBase"
                                 role="menuitem"
                             >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth="1.5"
-                                    stroke="currentColor"
-                                    className="size-4"
-                                >
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3"
-                                    />
-                                </svg>
+                                My profile
+                            </a>
 
-                                Logout
-                            </button>
-                        </form>
+                            <a
+                                href="#"
+                                className="block rounded-lg px-4 py-2 text-sm text-textBase hover:bg-bgBase hover:text-linkBase"
+                                role="menuitem"
+                            >
+                                Billing summary
+                            </a>
+
+                            <a
+                                href="#"
+                                className="block rounded-lg px-4 py-2 text-sm text-textBase hover:bg-bgBase hover:text-linkBase"
+                                role="menuitem"
+                            >
+                                Team settings
+                            </a>
+                        </div>
+
+                        <div className="p-2">
+                            <form method="POST" action="#">
+                                <button
+                                    type="submit"
+                                    className="flex w-full items-center gap-2 rounded-lg px-4 py-2 text-sm text-textBase hover:text-red-700 hover:bg-bgBase"
+                                    role="menuitem"
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        strokeWidth="1.5"
+                                        stroke="currentColor"
+                                        className="size-4"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3"
+                                        />
+                                    </svg>
+
+                                    Logout
+                                </button>
+                            </form>
+                        </div>
                     </div>
-                </div>
+                )}
             </div>
 
             <div className="block md:hidden">
