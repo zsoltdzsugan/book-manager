40f859cbaf4e275deade8b935bca64fc2bce829e
zsoltdzsugan <zsolt.dzsugan@gmail.com>
2024-10-04 06:05:32 +0200

    fix: style generalized

--- frontend/src/components/search/Searchbar.jsx
+++ frontend/src/components/search/Searchbar.jsx
@@ -1,17 +1,17 @@
 const Searchbar = () => {
     return (
-        <div className="relative w-full h-10">
+        <div className="relative w-full h-10 z-10 flex justify-center align-middle items-center">
             <label htmlFor="Search" className="sr-only">Search</label>
 
             <input
                 type="text"
                 id="Search"
                 placeholder="Search for..."
-                className="w-full rounded-2xl border-2 border-lightBorderBase dark:border-darkBorderBase py-2.5 pr-10 shadow-sm sm:text-sm px-3.5 bg-lightBgAccent lg:bg-lightBgContrast placeholder:text-darkBorderBase dark:bg-darkBgAccent dark:lg:bg-darkBgContrast dark:placeholder:text-lightBorderBase"
+                className="w-full flex rounded-2xl border-2 border-lightBorderBase dark:border-darkBorderBase py-2.5 pr-10 shadow-sm sm:text-sm px-3.5 bg-lightBgAccent lg:bg-lightBgContrast placeholder:text-darkBorderBase dark:bg-darkBgAccent dark:lg:bg-darkBgContrast dark:placeholder:text-lightBorderBase"
             />
 
             <span className="absolute inset-y-0 right-0 flex items-center pr-3">
-                <button type="button" className="text-textBase hover:text-linkBase py-1 px-0.5">
+                <button type="button" className="text-onyx lg:py-1 lg:px-0.5">
                     <span className="sr-only">Search</span>
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -19,7 +19,7 @@ const Searchbar = () => {
                         viewBox="0 0 24 24"
                         strokeWidth="2"
                         stroke="currentColor"
-                        className="w-5 h-5 stroke-lightTextBase hover:stroke-lightLinkBase dark:stroke-darkTextBase dark:hover:stroke-darkLinkBase"
+                        className="w-5 h-5 stroke-darkGreen hover:stroke-ultraViolet"
                     >
                         <path
                             strokeLinecap="round"
